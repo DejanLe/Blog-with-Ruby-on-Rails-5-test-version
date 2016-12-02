8e5bca64671ae0a8b620d6eca7da5308463ab26b
Dejanle <dejnik978@gmail.com>
2016-12-02 14:21:10 +0100

    integrate devise gem to users and posts

--- app/models/microspot.rb
+++ app/models/microspot.rb
@@ -1,5 +1,5 @@
 class Microspot < ApplicationRecord
-	 has_attached_file :image, styles: { medium: "160x160>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	 has_attached_file :image, styles: { medium: "160x160>", thumb: "325x475>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 	belongs_to :user
 	validates :content, length: {maximum: 1500 },
